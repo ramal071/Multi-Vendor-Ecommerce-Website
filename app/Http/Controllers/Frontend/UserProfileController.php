39e5ea014ad0ac50d0a3e5136b08b3a8d05b15e7
ramal071 <ramal.pathmila071@gamil.com>
2024-02-18 16:47:55 +0530

    dashboard ready

---
+++ app/Http/Controllers/Frontend/UserProfileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
+
+class UserProfileController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.dashboard.profile');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name'=>['required', 'max:100'],
+            'image'=>['image', 'max:2048'],
+            'email'=>['required', 'email', 'unique:users,email,'.Auth::user()->id]
+        ]);
+
+        $user = Auth::user();
+
+        if($request->hasFile('image')){
+            if(File::exists(public_path($user->image))){
+                File::delete(public_path($user->image));
+            }
+
+            $image = $request->image;
+            $imageName = rand().'_'.$image->getClientOriginalName();
+            $image->move(public_path('uploads'),$imageName);
+
+            $path = 'uploads/'.$imageName;
+
+            $user->image=$path;
+        }
+    }
+}
---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('dashboard',[AdminController::class, 'dashboard'])->name('dashboard');
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
